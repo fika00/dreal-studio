c6d377c1650bd02333401abc786b96415bd29d50
Filip R <filipradinovic2@gmail.com>
2023-09-03 23:40:14 +0200

    Change Logo

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -128,7 +128,7 @@ const LandingPage = ({ isPhone }) => {
       <div className="navbar">
         <img
           onClick={() => setOld(!old)}
-          src={old ? logo : logoOld}
+          src={old ? logoOld : old}
           alt=""
           className="logo"
           style={{
